1245ec12ff9a9cd25f9e1c81e67d26bdf43d993e
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-12 12:02:51 -0500

    commit tree 1bea4324948a1e6afe119f2e4b79d1662ccf7ec2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+katz_deli = []
+
+def line(x)
+    if x.empty?
+  puts "The line is currently empty."
+    else
+        z = x.collect.with_index(1) do |element, index|
+            "#{index}. #{element}"
+        end.join(" ")
+        puts "The line is currently: #{z}"
+    end
+end
+
+def take_a_number(x, name)
+    x.push(name)
+    puts "Welcome, #{name}. You are number #{x.length} in line."
+end
+
+def now_serving(x)
+    if x.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        dude = x.shift
+        puts "Currently serving #{dude}."
+    end
+end
+
+
